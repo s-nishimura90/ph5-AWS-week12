bf5a7ecf367637e875b54f2670bf5f26092766c7
s-nishimura90 <s-nishimura@joolen.com>
2024-04-06 00:03:08 +0900

    api.php修正

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
